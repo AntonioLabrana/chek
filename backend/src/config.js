80506f58b78c173128d7d4bd6d7ae9b71ea19898
Antonio Labraña <labrana.antonio@gmail.com>
2022-06-03 00:03:49 -0400

    Config BD Servidor Heroku

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,8 +3,8 @@ import { config } from "dotenv";
 config();
 
 export default{
-    host: process.env.HOST || "",
-    database: process.env.DATABASE || "",
-    user: process.env.USER || "",
-    password: process.env.PASSWORD || ""
+    host: process.env.HOST || "localhost",
+    database: process.env.DATABASE || "check",
+    user: process.env.USER || "check",
+    password: process.env.PASSWORD || "check"
 }
