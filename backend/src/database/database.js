f348203141f7c9841f9875c0b95e17b8d61476fb
Antonio Labraña <labrana.antonio@gmail.com>
2022-06-02 23:32:59 -0400

    Configuracion DB Heroku

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,10 +2,10 @@ import mysql from "promise-mysql";
 import config from "./../config.js";
 
 const connection = mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.user,
-    password: config.password
+    host: 'us-cdbr-east-05.cleardb.net' || config.host,
+    database: 'heroku_39fd0a6f653f72b' || config.database,
+    user: 'baf344f5e0d783' || config.user,
+    password: '3f967d0a' || config.password
 });
 
 export const getConnection = () => {
